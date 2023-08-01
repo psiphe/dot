251b47d24a6b6132fdbf5bec2b5bb21dca0485b5
sayeef <sayeef@sayeefs-MacBook-Air.local>
2023-07-31 23:08:27 -0700

    incr

--- lisp/user-dired.el
+++ lisp/user-dired.el
@@ -10,5 +10,12 @@
   (:map dired-mode-map
         ("p" . dired-up-directory)))
 
+(use-package dirvish
+  :init
+  (dirvish-override-dired-mode)
+  :bind
+  (:map dirvish-mode-map
+        ("'" . dirvish-layout-toggle)))
+
 (provide 'user-dired)
 ;;; user-dired.el ends here
